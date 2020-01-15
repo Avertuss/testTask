7aea33a20e325e6820035773e0fe2d1f65e56a2f
Avertuss <kurrustam@yandex.ru>
2020-01-16 00:15:02 +0300

    add logick close open block

--- build/script.js
+++ build/script.js
@@ -1,16 +1,16 @@
-document.body.addEventListener('click',function(event){
-
-var element = event.target;
-while(element && !element.classList.contains('history__transaction'))
-{
-    element = element.parentElement;
-}
-if(element && element.children[1])
-{
-    if(element.children[1].classList.contains('history__hide'))
-        element.children[1].classList.replace('history__hide','history__show');
-    else 
-        element.children[1].classList.replace('history__show','history__hide');
-}
-
-})
+function changeListener(event){
+    var element = event.target;
+        while(element && !element.classList.contains('history__transaction'))
+        {
+            element = element.parentElement;
+        }
+        if(element && element.children[1])
+        {
+            if(element.children[1].classList.contains('history__hide'))
+                element.children[1].classList.replace('history__hide','history__show');
+            else 
+                element.children[1].classList.replace('history__show','history__hide');
+        }
+    
+    }
+document.body.addEventListener('click',changeListener);
