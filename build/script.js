e55de20e30861aca8dd374e790a5d6222911fbf2
Avertuss <kurrustam@yandex.ru>
2020-01-15 17:50:52 +0300

    add logick close open block

--- build/script.js
+++ build/script.js
@@ -1,4 +1,16 @@
 document.body.addEventListener('click',function(event){
-console.log(event.target);
+
+var element = event.target;
+while(element && !element.classList.contains('history__transaction'))
+{
+    element = element.parentElement;
+}
+if(element && element.children[1])
+{
+    if(element.children[1].classList.contains('history__hide'))
+        element.children[1].classList.replace('history__hide','history__show');
+    else 
+        element.children[1].classList.replace('history__show','history__hide');
+}
 
 })
